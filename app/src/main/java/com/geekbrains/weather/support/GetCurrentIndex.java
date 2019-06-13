34f1f0ea30ace01a2b0615416538669f923925d8
rawender <rawender@yandex.ru>
2019-06-13 15:27:59 +0300

    task for lesson 6 (задание к уроку 6)

---
+++ app/src/main/java/com/geekbrains/weather/support/GetCurrentIndex.java
@@ -0,0 +1,5 @@
+package com.geekbrains.weather.support;
+
+public interface GetCurrentIndex {
+    void getCurrentIndex(int index);
+}
---
+++ app/src/main/java/com/geekbrains/weather/support/entites/WeatherRestModel.java
@@ -0,0 +1,10 @@
+package com.geekbrains.weather.support.entites;
+
+import com.google.gson.annotations.SerializedName;
+
+public class WeatherRestModel {
+    @SerializedName("id") public int id;
+    @SerializedName("main") public String main;
+    @SerializedName("description") public String description;
+    @SerializedName("icon") public String icon;
+}
